ba1a6f349f659d2f94188ebd7c8447eafbc49c3f
Zamirathe <zamirathe@centurylink.net>
2015-03-23 12:31:29 -0600

    Release 1.0.0.1

---
+++ autosave/AutoSave.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Rocket.RocketAPI;
+using SDG;
+using UnityEngine;
+
+namespace AutoSave
+{
+    internal class AutoSave : RocketPlugin<AutoSaveConfiguration>
+    {
+        public DateTime start = DateTime.Now;
+        public bool msgYorN;
+
+        public void save() {
+            if (!this.Configuration.Enabled)
+            {
+                return;
+            }
+                
+            if (this.Loaded != false && this.start != null && (DateTime.Now - this.start).TotalSeconds > (double) this.Configuration.SaveInterval)
+            {
+                if (!this.msgYorN)
+                {
+                    RocketChatManager.Say(this.Configuration.SaveMsg, (EChatMode)0);
+                    this.msgYorN = true;
+                }
+                SaveManager.save();
+                this.msgYorN = false;
+                this.start = DateTime.Now;
+                Debug.Log("Saved at " + DateTime.Now);
+            }
+        }
+        public void FixedUpdate()
+        {
+            this.save();
+        }
+    }
+}
---
+++ autosave/AutoSaveConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Rocket.RocketAPI;
+
+namespace AutoSave
+{
+    public class AutoSaveConfiguration : RocketConfiguration
+    {
+        public bool Enabled;
+        public int SaveInterval;
+        public string SaveMsg;
+
+        public RocketConfiguration DefaultConfiguration 
+        {
+            get 
+            {
+                AutoSaveConfiguration configuration = new AutoSaveConfiguration();
+                configuration.Enabled = true;
+                configuration.SaveInterval = 1800;
+                configuration.SaveMsg = "Saving world...";
+            return (RocketConfiguration) configuration;
+            }
+        }
+        public AutoSaveConfiguration() : base()
+        {
+            return;
+        }
+    }
+}
